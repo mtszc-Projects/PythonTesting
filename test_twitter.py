8ed3ad6c199c84b2c785eb06d2d4d33ed832be23
stephendedalus46 <cieciur@onet.pl>
2020-07-11 17:24:39 +0200

    github API request

--- test_twitter.py
+++ test_twitter.py
@@ -19,18 +19,18 @@ def fixture_twitter(backend, request):
 
 
 def test_twitter_initialization(twitter):
-    assert twitter  # assert checks if expresion after it is true
+    assert twitter
 
 
 def test_tweet_single_message(twitter):
     twitter.tweet('Test message')
-    assert twitter.tweets == ['Test message']
+    assert twitter.tweet_messages == ['Test message']
 
 
 def test_tweet_long_message(twitter):
     with pytest.raises(Exception):
         twitter.tweet('test'*41)
-    assert twitter.tweets == []
+    assert twitter.tweet_messages == []
 
 
 def test_initialize_two_twitter_classes(backend):
@@ -40,7 +40,7 @@ def test_initialize_two_twitter_classes(backend):
     twitter1.tweet('Test 1')
     twitter1.tweet('Test 2')
 
-    assert twitter2.tweets == ['Test 1', 'Test 2']
+    assert twitter2.tweet_messages == ['Test 1', 'Test 2']
 
 
 @pytest.mark.parametrize("message, expected", (
--- twitter.py
+++ twitter.py
@@ -1,37 +1,45 @@
 import re
+import json
+import requests
+from urllib.parse import urljoin
+
+
+USERS_API = "https://api.github.com/users/"
 
 
 class Twitter:
     version = '1.0'
 
-    def __init__(self, backend=None):
+    def __init__(self, backend=None, username=None):
         self.backend = backend
         self._tweets = []
-        # moved to test file as a fixture
-        # if self.backend and not os.path.exists(self.backend):
-        #     with open(self.backend, 'w'):
-        #         pass
-
-    # def delete(self):
-    #     if self.backend:
-    #         os.remove(self.backend)
+        self.username = username
 
     @property
     def tweets(self):
         if self.backend and not self._tweets:
-            # with open(self.backend) as twitter_file:
-            #     self._tweets = [line.rstrip('\n') for line in twitter_file]
-            self._tweets = [line.rstrip('\n') for line in self.backend.readlines()]
+            backend_text = self.backend.read()
+            if backend_text:
+                self._tweets = json.loads(backend_text)
         return self._tweets
+
+    @property
+    def tweet_messages(self):
+        return [tweet['message'] for tweet in self.tweets]
+
+    def get_user_avatar(self):
+        if not self.username:
+            return None
+        url = urljoin(USERS_API, self.username)
+        resp = requests.get(url)
+        return resp.json()['avatar_url']
 
     def tweet(self, message):
         if len(message) > 160:
             raise Exception("Message too long.")
-        self.tweets.append(message)
+        self.tweets.append({'message': message, 'avatar': self.get_user_avatar()})
         if self.backend:
-            # with open(self.backend, 'w') as twitter_file:
-            #     twitter_file.write('\n'.join(self.tweets))
-            self.backend.write('\n'.join(self.tweets))
+            self.backend.write(json.dumps(self.tweets))
 
     @staticmethod
     def find_hashtags(message):
